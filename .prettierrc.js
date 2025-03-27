7a57ec8fd07d8c89ef333a7b6e60d8ba3d7e47a3
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2025-03-27 20:38:46 +0700

    config project

---
+++ .prettierrc.js
@@ -0,0 +1,34 @@
+module.exports = {  
+  // Indentation uses 2 spaces per level  
+  tabWidth: 2,  
+
+  // Maximum line length before wrapping  
+  printWidth: 120,  
+
+  // Use spaces for indentation, not tabs  
+  useTabs: false,  
+
+  // Add semicolons at the end of statements  
+  semi: true,  
+
+  // Use double quotes in JSX attributes  
+  jsxSingleQuote: false,  
+
+  // Add trailing commas wherever possible (arrays, objects)  
+  trailingComma: "all",  
+
+  // Always include parentheses around arrow function parameters  
+  arrowParens: "always",  
+
+  // Use double quotes for strings  
+  singleQuote: false,  
+
+  // Add spaces between brackets in object literals  
+  bracketSpacing: true,  
+
+  // JSX closing brackets go on a new line  
+  bracketSameLine: false,  
+
+  // Automatically detect and maintain consistent line endings  
+  endOfLine: "auto",  
+}
